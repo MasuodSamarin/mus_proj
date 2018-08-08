7dd4024a5395627dc89174b72e7316cfcb2675cc
sam <bighat.bighead@gmail.com>
2018-08-08 23:47:11 +0430

    complete volume module

--- APPS/encoder/encoder.c
+++ APPS/encoder/encoder.c
@@ -14,17 +14,28 @@
 /*
  * static variables
  * */
-
 static uint32_t enc_old_val = 0;
-
-
-
-
 
 void enc_init(void){
 	MX_TIM4_Init();
 	HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_ALL);
 	enc_set_span(20);
+}
+
+void enc_set_span(uint32_t span){
+	__HAL_TIM_SET_AUTORELOAD(&htim4, span);
+}
+
+uint32_t enc_get_span(void){
+	return(__HAL_TIM_GET_AUTORELOAD(&htim4));
+}
+
+void enc_set_val(uint32_t val){
+	__HAL_TIM_SET_COUNTER(&htim4, val);
+}
+
+uint32_t enc_get_val(void){
+	return(__HAL_TIM_GET_COUNTER(&htim4));
 }
 
 void enc_process(void){
@@ -56,17 +67,4 @@ void enc_process(void){
 	event_push_node(event_create_enc_node(dir, delta));
 }
 
-void enc_set_span(uint32_t span){
-	__HAL_TIM_SET_AUTORELOAD(&htim4, span);
-}
 
-uint32_t enc_get_span(void){
-	return(__HAL_TIM_GET_AUTORELOAD(&htim4));
-}
-
-void enc_set_val(uint32_t val){
-	__HAL_TIM_SET_COUNTER(&htim4, val);
-}
-uint32_t enc_get_val(void){
-	return(__HAL_TIM_GET_COUNTER(&htim4));
-}
