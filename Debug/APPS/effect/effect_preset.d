6c9d818c94ab942134c0f4610a5399e966666ead
Masuod <bighat.bighead@gmail.com>
2018-08-27 01:43:13 +0430

    i dont get it why debug

---
+++ Debug/APPS/effect/effect_preset.d
@@ -0,0 +1,117 @@
+APPS/effect/effect_preset.o: ../APPS/effect/effect_preset.c \
+ /home/sam/w/musavi/mus_proj/APPS/fv1/fv1.h \
+ /home/sam/w/musavi/mus_proj/Inc/main.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/sam/w/musavi/mus_proj/Inc/stm32f1xx_hal_conf.h \
+ /home/sam/w/musavi/mus_proj/Inc/main.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cm3.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ /home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ /home/sam/w/musavi/mus_proj/Inc/gpio.h ../APPS/effect/effect.h \
+ /home/sam/w/musavi/mus_proj/APPS/event/event.h \
+ /home/sam/w/musavi/mus_proj/Inc/adc.h \
+ /home/sam/w/musavi/mus_proj/Inc/dma.h \
+ /home/sam/w/musavi/mus_proj/Inc/spi.h \
+ /home/sam/w/musavi/mus_proj/Inc/tim.h \
+ /home/sam/w/musavi/mus_proj/APPS/link_list/link_list.h \
+ /home/sam/w/musavi/mus_proj/APPS/push-btn/push_btn.h \
+ /home/sam/w/musavi/mus_proj/APPS/encoder/encoder.h \
+ /home/sam/w/musavi/mus_proj/APPS/volume/volume.h
+
+/home/sam/w/musavi/mus_proj/APPS/fv1/fv1.h:
+
+/home/sam/w/musavi/mus_proj/Inc/main.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/sam/w/musavi/mus_proj/Inc/stm32f1xx_hal_conf.h:
+
+/home/sam/w/musavi/mus_proj/Inc/main.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cm3.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+/home/sam/w/musavi/mus_proj/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+/home/sam/w/musavi/mus_proj/Inc/gpio.h:
+
+../APPS/effect/effect.h:
+
+/home/sam/w/musavi/mus_proj/APPS/event/event.h:
+
+/home/sam/w/musavi/mus_proj/Inc/adc.h:
+
+/home/sam/w/musavi/mus_proj/Inc/dma.h:
+
+/home/sam/w/musavi/mus_proj/Inc/spi.h:
+
+/home/sam/w/musavi/mus_proj/Inc/tim.h:
+
+/home/sam/w/musavi/mus_proj/APPS/link_list/link_list.h:
+
+/home/sam/w/musavi/mus_proj/APPS/push-btn/push_btn.h:
+
+/home/sam/w/musavi/mus_proj/APPS/encoder/encoder.h:
+
+/home/sam/w/musavi/mus_proj/APPS/volume/volume.h:
---
+++ Debug/APPS/fsme/fsme-test.d
@@ -0,0 +1,3 @@
+APPS/fsme/fsme-test.o: ../APPS/fsme/fsme-test.c ../APPS/fsme/fsme.h
+
+../APPS/fsme/fsme.h:
